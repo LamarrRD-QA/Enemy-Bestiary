3201eabc279e62db0792adb676845ffd649130ce
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-05-07 09:21:58 +0100

    LRD: Added initial project

---
+++ src/main/java/com/qa/enemybestiary/EnemyBestiaryApplication.java
@@ -0,0 +1,13 @@
+package com.qa.enemybestiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnemyBestiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnemyBestiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/enemybestiary/EnemyBestiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.enemybestiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnemyBestiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

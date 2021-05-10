1475b05ef6029916cb4b6d38d9e92167bf6b4d08
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-05-10 17:53:35 +0100

    EBL-7 #comment Cleanup Catalogue script

--- src/main/resources/static/js/catalogue.js
+++ src/main/resources/static/js/catalogue.js
@@ -20,31 +20,27 @@
 
         // Create node for enemy image and append to row
         let enemyImg = document.createElement(`img`);
+        let columnImg = document.createElement(`td`);
         enemyImg.setAttribute(`src`, `../res/table_enemy/${enemy.name}.png`);
         enemyImg.setAttribute(`alt`, `Image of ${enemy.name}`);
-        let columnImg = document.createElement(`th`);
-        columnImg.setAttribute(`scope`, `col`);
         columnImg.appendChild(enemyImg);
         row.appendChild(columnImg);
 
         // Create node for enemy name and append to row
         let enemyName = document.createTextNode(enemy.name);
-        let columnName = document.createElement(`th`);
-        columnName.setAttribute(`scope`, `col`);
+        let columnName = document.createElement(`td`);
         columnName.appendChild(enemyName);
         row.appendChild(columnName);
 
         // Create node for enemy type and append to row
         let enemyType = document.createTextNode(enemy.enemyType);
-        let columnType = document.createElement(`th`);
-        columnType.setAttribute(`scope`, `col`);
+        let columnType = document.createElement(`td`);
         columnType.appendChild(enemyType);
         row.appendChild(columnType);
 
         // Create node for whether enemy is normal or custom and append to row
         let enemyIsCustom = enemy.isCustom ? document.createTextNode(`Custom`) : document.createTextNode(`Normal`);
-        let columnIsCustom = document.createElement(`th`);
-        columnIsCustom.setAttribute(`scope`, `col`);
+        let columnIsCustom = document.createElement(`td`);
         columnIsCustom.appendChild(enemyIsCustom);
         row.appendChild(columnIsCustom);
 

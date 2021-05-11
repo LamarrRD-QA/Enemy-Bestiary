93f68f97228e00f7bb2affd288909461d0b709d0
Lamarr Redhead-Davis <LRedhead-Davis@qa.com>
2021-05-11 04:50:12 +0100

    EBL-8 #comment Add Lombok functionality to Enemy

--- src/main/java/com/qa/enemybestiary/domain/Enemy.java
+++ src/main/java/com/qa/enemybestiary/domain/Enemy.java
@@ -2,8 +2,6 @@ package com.qa.enemybestiary.domain;
 
 import java.math.BigDecimal;
 import java.util.EnumSet;
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,7 +14,12 @@ import com.qa.enemybestiary.domain.enums.Item;
 import com.qa.enemybestiary.domain.enums.Location;
 import com.qa.enemybestiary.domain.enums.MeleeReaction;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
 public class Enemy {
 
 	@Id
@@ -54,9 +57,6 @@ public class Enemy {
 	private BigDecimal itemTwoChance;
 	private Boolean isCustom;
 
-	public Enemy() {
-	}
-
 	public Enemy(String name, EnemyType enemyType, EnumSet<Location> locations, @NotNull Integer level,
 			@NotNull Integer maxHP, @NotNull Integer pow, @NotNull Integer def, @NotNull Integer speed,
 			MeleeReaction jump, MeleeReaction hammer, MeleeReaction hand, ElementalReaction fire,
@@ -86,228 +86,6 @@ public class Enemy {
 		this.itemOneChance = itemOneChance;
 		this.itemTwo = itemTwo;
 		this.itemTwoChance = itemTwoChance;
-		this.isCustom = isCustom;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(chanceOfBurn, chanceOfStatDown, chanceOfStun, coins, def, exp, fire, hammer, hand, id,
-				isCustom, itemOne, itemOneChance, itemTwo, itemTwoChance, jump, level, locations, maxHP, name, pow,
-				speed, thunder, enemyType);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Enemy other = (Enemy) obj;
-		return Objects.equals(chanceOfBurn, other.chanceOfBurn)
-				&& Objects.equals(chanceOfStatDown, other.chanceOfStatDown)
-				&& Objects.equals(chanceOfStun, other.chanceOfStun) && Objects.equals(coins, other.coins)
-				&& Objects.equals(def, other.def) && Objects.equals(exp, other.exp) && fire == other.fire
-				&& hammer == other.hammer && hand == other.hand && Objects.equals(id, other.id)
-				&& Objects.equals(isCustom, other.isCustom) && itemOne == other.itemOne
-				&& Objects.equals(itemOneChance, other.itemOneChance) && itemTwo == other.itemTwo
-				&& Objects.equals(itemTwoChance, other.itemTwoChance) && jump == other.jump
-				&& Objects.equals(level, other.level) && Objects.equals(locations, other.locations)
-				&& Objects.equals(maxHP, other.maxHP) && Objects.equals(name, other.name)
-				&& Objects.equals(pow, other.pow) && Objects.equals(speed, other.speed) && thunder == other.thunder
-				&& enemyType == other.enemyType;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public EnemyType getEnemyType() {
-		return enemyType;
-	}
-
-	public EnumSet<Location> getLocations() {
-		return locations;
-	}
-
-	public Integer getLevel() {
-		return level;
-	}
-
-	public Integer getMaxHP() {
-		return maxHP;
-	}
-
-	public Integer getPow() {
-		return pow;
-	}
-
-	public Integer getDef() {
-		return def;
-	}
-
-	public Integer getSpeed() {
-		return speed;
-	}
-
-	public MeleeReaction getJump() {
-		return jump;
-	}
-
-	public MeleeReaction getHammer() {
-		return hammer;
-	}
-
-	public MeleeReaction getHand() {
-		return hand;
-	}
-
-	public ElementalReaction getFire() {
-		return fire;
-	}
-
-	public ElementalReaction getThunder() {
-		return thunder;
-	}
-
-	public BigDecimal getChanceOfStun() {
-		return chanceOfStun;
-	}
-
-	public BigDecimal getChanceOfBurn() {
-		return chanceOfBurn;
-	}
-
-	public BigDecimal getChanceOfStatDown() {
-		return chanceOfStatDown;
-	}
-
-	public Integer getExp() {
-		return exp;
-	}
-
-	public Integer getCoins() {
-		return coins;
-	}
-
-	public Item getItemOne() {
-		return itemOne;
-	}
-
-	public BigDecimal getItemOneChance() {
-		return itemOneChance;
-	}
-
-	public Item getItemTwo() {
-		return itemTwo;
-	}
-
-	public BigDecimal getItemTwoChance() {
-		return itemTwoChance;
-	}
-
-	public Boolean getIsCustom() {
-		return isCustom;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setEnemyType(EnemyType enemyType) {
-		this.enemyType = enemyType;
-	}
-
-	public void setLocations(EnumSet<Location> locations) {
-		this.locations = locations;
-	}
-
-	public void setLevel(Integer level) {
-		this.level = level;
-	}
-
-	public void setMaxHP(Integer maxHP) {
-		this.maxHP = maxHP;
-	}
-
-	public void setPow(Integer pow) {
-		this.pow = pow;
-	}
-
-	public void setDef(Integer def) {
-		this.def = def;
-	}
-
-	public void setSpeed(Integer speed) {
-		this.speed = speed;
-	}
-
-	public void setJump(MeleeReaction jump) {
-		this.jump = jump;
-	}
-
-	public void setHammer(MeleeReaction hammer) {
-		this.hammer = hammer;
-	}
-
-	public void setHand(MeleeReaction hand) {
-		this.hand = hand;
-	}
-
-	public void setFire(ElementalReaction fire) {
-		this.fire = fire;
-	}
-
-	public void setThunder(ElementalReaction thunder) {
-		this.thunder = thunder;
-	}
-
-	public void setChanceOfStun(BigDecimal chanceOfStun) {
-		this.chanceOfStun = chanceOfStun;
-	}
-
-	public void setChanceOfBurn(BigDecimal chanceOfBurn) {
-		this.chanceOfBurn = chanceOfBurn;
-	}
-
-	public void setChanceOfStatDown(BigDecimal chanceOfStatDown) {
-		this.chanceOfStatDown = chanceOfStatDown;
-	}
-
-	public void setExp(Integer exp) {
-		this.exp = exp;
-	}
-
-	public void setCoins(Integer coins) {
-		this.coins = coins;
-	}
-
-	public void setItemOne(Item itemOne) {
-		this.itemOne = itemOne;
-	}
-
-	public void setItemOneChance(BigDecimal itemOneChance) {
-		this.itemOneChance = itemOneChance;
-	}
-
-	public void setItemTwo(Item itemTwo) {
-		this.itemTwo = itemTwo;
-	}
-
-	public void setItemTwoChance(BigDecimal itemTwoChance) {
-		this.itemTwoChance = itemTwoChance;
-	}
-
-	public void setIsCustom(Boolean isCustom) {
 		this.isCustom = isCustom;
 	}
 
